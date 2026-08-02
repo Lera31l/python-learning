d8cd8ba417e5a7178486635d088417a5dc92b3c9
Lera31l <132395297+Lera31l@users.noreply.github.com>
2026-08-02 10:16:40 +0500

    Add files via upload

---
+++ day1_variables.py
@@ -0,0 +1,32 @@
+
+
+name = "Lera"
+age = 24
+height = 1.65
+im_Learning_Python_now = True
+
+print(name)
+print(age)
+print(height)
+print(im_Learning_Python_now)
+
+a = 24
+b = 23
+
+print(" ")
+
+print("Сумма:", a+b)
+print("Разность:", a-b)
+print("Произведение:", a*b)
+print("Частное:", a//b)
+
+my_game = "Void Cleaner"
+neuro_shards = 1.234
+print("Я разрабатываю игру:", my_game)
+print("У игрока сейчас:", neuro_shards, "NeuroShards")
+
+print(type(my_game))
+print(type(neuro_shards))
+print(type(name))
+print(type(age))
+print(type(height))
