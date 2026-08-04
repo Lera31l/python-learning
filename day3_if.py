c0240aab34d6b7b737ba3f26850b3800ad2ed1f1
Lera31l <132395297+Lera31l@users.noreply.github.com>
2026-08-04 13:46:01 +0500

    Add files via upload

---
+++ day3_if.py
@@ -0,0 +1,34 @@
+
+# =====Задание 1 =====
+
+age = input("Сколко тебе лет? ")
+age = int(age)
+
+if age >= 18:
+    print("Доступ разрешён")
+else:
+    print("Доступ запрещён")
+
+
+# ===== Задание 2 ====
+
+shards = input("Сколько у тебя NeuroShards?  ")
+shards = int(shards)
+
+if shards >= 100:
+    print("Можно купить улучшение оружия")
+else:
+    print("Нужно ещё фармить")
+
+# ==== Задание 3 ====
+
+number = input("Напиши число: ")
+number = int(number)
+
+if number > 0:
+    print("Положительное")
+elif number == 0:
+    print("Ноль")
+else:
+    print("Отрицательное")
+
