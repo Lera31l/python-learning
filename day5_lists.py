15318bf374f95973d76dc1a5b55acf989a11c7fd
Lera31l <132395297+Lera31l@users.noreply.github.com>
2026-08-04 14:41:36 +0500

    Add files via upload

---
+++ day5_lists.py
@@ -0,0 +1,28 @@
+
+# Создаём список героев
+heroes = ["Purge", "Reaver", "Specter", "Bastion"]
+
+# Выводим каждого по отдельности
+print(heroes [0])
+print(heroes [1])
+print(heroes [2])
+print(heroes [3])
+
+# Узнаём, сколько героев
+print ("Всего героев: ", len(heroes))
+
+# Добавляем нового героя
+new_hero = input("Введи имя нового героя: ")
+heroes.append(new_hero)
+
+# Смотрим, что получилось
+print("Обновлённый список героев:")
+print(heroes)
+
+
+# Выводим первого и последнего
+print("Первый герой:", heroes[0])
+print("Последний герой:", heroes[-1])   # -1 всегда означает последний элемент
+
+weapons = ["Purge", "Reaver", "Specter", "Bastion"]
+print("Всего оружия: ",len(weapons))
